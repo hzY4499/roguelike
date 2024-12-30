de34bfcb122f153df5c86b258ddfb605abd1f133
LBSqc9 <2143193206@qq.com>
2024-12-30 12:49:37 +0800

    敌人撞墙不死亡bug修复

--- Assets/Scripts/Enemy_0.cs
+++ Assets/Scripts/Enemy_0.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class Enemy_0 : MonoBehaviour
 {
-    // �ƶ��ٶ�
     [SerializeField] private float speed;
 
-    // �����ʼ��Զǽ�ڷ���
     private Vector3 moveDirection;
     [SerializeField] private ParticleSystem spawnIndicator;
     [SerializeField] private SpriteRenderer enemyRenderer;
     [SerializeField] private ParticleSystem passAwayParticles;
     float randomX;
     float randomY;
-    // Start ����Ϸ��ʼʱ����
+
     void Start()
     {       
         RandomLocation();
@@ -24,10 +22,8 @@ public class Enemy_0 : MonoBehaviour
             .setOnComplete(SpawnSequenceCompleted);
     }
 
-    // Update ÿһ֡����
     void Update()
     {
-        // ��������Զ��ǽ�ڷ����ƶ�
         transform.position += moveDirection * speed * Time.deltaTime;
     }
 
@@ -41,10 +37,8 @@ public class Enemy_0 : MonoBehaviour
 
     private void RandomLocation()
     {
-        // ȷ�����������ڱ�Ե
         if (Random.value > 0.5f)
         {
-            // ��������߻��ұ߱�Ե
             if (Random.value > 0.5f)
             {
                 randomX = -4.5f;
@@ -55,11 +49,10 @@ public class Enemy_0 : MonoBehaviour
                 randomX = 4.5f;
                 moveDirection = Vector3.left;
             }
-            randomY = Random.Range(-4f, 4f); // ���±�Ե���
+            randomY = Random.Range(-4f, 4f); 
         }
         else
         {
-            // �������ϱ߻��±߱�Ե
             if (Random.value > 0.5f)
             {
                 randomY = -4f;
@@ -70,39 +63,9 @@ public class Enemy_0 : MonoBehaviour
                 randomY = 4f;
                 moveDirection = Vector3.down;
             }
-            randomX = Random.Range(-4.5f, 4.5f); // ���ұ�Ե���
+            randomX = Random.Range(-4.5f, 4.5f); 
         }
 
         transform.position = new Vector3(randomX, randomY, 0);
-    }
-
-    // �����ǽ�ڵ���ײ����ʧ
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // �����ײ�Ķ�����ǽ�ڣ�������ʧ
-        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Player"))
-        {
-            // ���ٵ��˶���
-            PassAway();
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // �����ײ�����Ƿ����ӵ�
-        if (collision.gameObject.CompareTag("Bullet"))
-        {
-            // �����ӵ�
-            // Destory(collision.gameObject);
-
-            transform.position = new Vector3(randomX, randomY, 0);
-        }
-    }
-
-    private void PassAway()
-    {
-
-        passAwayParticles.Play();
-            passAwayParticles.transform.parent = null;
-        Destroy(gameObject);
     }
 }
